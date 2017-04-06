e07d3994b23dd58de5cfa0cbd2c435b539cb9df1
Emerson Macedo <emerleite@gmail.com>
2017-04-06 11:37:41 -0300

    First elixir commit

---
+++ lib/hodor.ex
@@ -0,0 +1,18 @@
+defmodule Hodor do
+  @moduledoc """
+  Documentation for Hodor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hodor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hodor_test.exs
@@ -0,0 +1,8 @@
+defmodule HodorTest do
+  use ExUnit.Case
+  doctest Hodor
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
